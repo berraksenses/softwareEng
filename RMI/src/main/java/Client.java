093b8742f7f369ba2c97a6ca807dc549945afb50
berraksenses <studente@localhost.localdomain>
2021-04-06 07:02:50 +0200

    “”

---
+++ RMI/src/main/java/Client.java
@@ -0,0 +1,31 @@
+
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Arrays;
+
+/**
+ *
+ * @author studente
+ */
+public class Client {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        Registry registry = LocateRegistry.getRegistry("localhost", 5555);
+        ServerInterface stub = (ServerInterface) registry.lookup("Server");
+        int myID = stub.startTask();
+        Thread.sleep(4500);
+        while ( stub.isReady(myID) ) 
+        {
+            System.out.println("... sto aspettando ...");
+            Thread.sleep(4500);
+        }
+        System.out.println(Arrays.toString(stub.getResults(myID)));
+    }
+    
+    
+}
